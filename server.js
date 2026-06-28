b3e922a5f6ff8877320742a6d8d10de61555c2e4
DeveshGoswami789 <deveshgiri.goswami123@gmail.com>
2026-06-28 14:54:38 +0530

    Fixing

--- server.js
+++ server.js
@@ -4,52 +4,29 @@ const { Server } = require("socket.io");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const db = require("./db");
-
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-
 app.use(express.static("public"));
+
 if (!fs.existsSync("uploads")) {
     fs.mkdirSync("uploads");
 }
-app.use(
-    "/uploads",
-    express.static("uploads")
-);
 
-const storage =
-multer.diskStorage({
+app.use("/uploads", express.static("uploads"));
 
-    destination:
-    (req,file,cb)=>{
-
-        cb(
-            null,
-            "uploads/"
-        );
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
     },
-
-    filename:
-    (req,file,cb)=>{
-
-        cb(
-            null,
-            Date.now() +
-            path.extname(
-                file.originalname
-            )
-        );
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 
-const upload =
-multer({
-    storage
-});
+const upload = multer({ storage });
 
 let users = 0;
 let onlineUsers = [];
@@ -57,232 +34,142 @@ let seenData = {};
 let reactions = {};
 let messages = {};
 
-
-app.post(
-"/upload",
-upload.single("file"),
-(req,res)=>{
-
+app.post("/upload", upload.single("file"), (req, res) => {
     res.json({
-        url:
-        "/uploads/" +
-        req.file.filename
+        url: "/uploads/" + req.file.filename
     });
-
 });
 
 io.on("connection", (socket) => {
+
     users++;
     io.emit("users count", users);
 
     console.log("User connected");
-db.all(
-    "SELECT * FROM messages ORDER BY time ASC",
-    [],
-    (err, rows) => {
-
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        socket.emit("chat history", rows);
-    }
-);
 
     socket.on("new user", (username) => {
-    socket.username = username;
 
-    onlineUsers.push(username);
+        socket.username = username;
 
-    io.emit("online users", onlineUsers);
+        onlineUsers.push(username);
 
-    io.emit("system", username + " joined the chat");
-});
+        io.emit("online users", onlineUsers);
 
-    socket.on("chat message", async (data) => {
+        io.emit("system", username + " joined the chat");
+    });
 
-    messages[data.id] = {
-        user: socket.username,
-        text: data.text
-    };
+    socket.on("chat message", (data) => {
 
-    db.run(
-    `INSERT INTO messages
-    (id,user,text,image,audio,time,reply)
-    VALUES (?,?,?,?,?,?,?)`,
-    [
-        data.id,
-        data.user,
-        data.text || null,
-        data.image || null,
-        data.audio || null,
-        data.time,
-        data.reply || null
-    ]
-);
+        messages[data.id] = {
+            user: socket.username,
+            text: data.text
+        };
 
-    io.emit(
-        "chat message",
-        data
-    );
-});
+        io.emit("chat message", data);
+    });
 
-socket.on(
-"message seen",
-(data)=>{
+    socket.on("message seen", (data) => {
 
-if(
-!seenData[data.messageId]
-){
-seenData[data.messageId] = [];
-}
+        if (!seenData[data.messageId]) {
+            seenData[data.messageId] = [];
+        }
 
-if(
-!seenData[data.messageId]
-.includes(data.user)
-){
+        if (!seenData[data.messageId].includes(data.user)) {
+            seenData[data.messageId].push(data.user);
+        }
 
-seenData[data.messageId]
-.push(data.user);
-}
+        io.emit("message seen", {
+            messageId: data.messageId,
+            users: seenData[data.messageId]
+        });
 
-io.emit(
-"message seen",
-{
-messageId:
-data.messageId,
+    });
 
-users:
-seenData[
-data.messageId
-]
-}
-);
+    socket.on("delete message", (messageId) => {
 
-});
-
-socket.on(
-    "delete message",
-    (messageId) => {
-
-        if(
+        if (
             messages[messageId] &&
             messages[messageId].user === socket.username
-        ){
+        ) {
 
             delete messages[messageId];
 
-            io.emit(
-                "delete message",
-                messageId
-            );
+            io.emit("delete message", messageId);
         }
-    }
-);
-socket.on(
-    "reaction",
-    (data) => {
 
-        const {
-            messageId,
-            emoji
-        } = data;
+    });
 
-        if(
-            !reactions[messageId]
-        ){
+    socket.on("reaction", (data) => {
+
+        const { messageId, emoji } = data;
+
+        if (!reactions[messageId]) {
             reactions[messageId] = {};
         }
 
-        if(
-            !reactions[messageId][emoji]
-        ){
+        if (!reactions[messageId][emoji]) {
             reactions[messageId][emoji] = [];
         }
 
-        const users =
-        reactions[messageId][emoji];
+        const users = reactions[messageId][emoji];
 
-        const index =
-        users.indexOf(
-            socket.username
-        );
+        const index = users.indexOf(socket.username);
 
-        if(index > -1){
-
-            users.splice(
-                index,
-                1
-            );
-
-        }else{
-
-            users.push(
-                socket.username
-            );
+        if (index > -1) {
+            users.splice(index, 1);
+        } else {
+            users.push(socket.username);
         }
 
-        io.emit(
-            "reaction update",
-            {
-                messageId,
-                reactions:
-                reactions[messageId]
-            }
-        );
-    }
-);
+        io.emit("reaction update", {
+            messageId,
+            reactions: reactions[messageId]
+        });
 
-socket.on(
-    "edit message",
-    (data) => {
+    });
 
-        if(
+    socket.on("edit message", (data) => {
+
+        if (
             messages[data.id] &&
             messages[data.id].user === socket.username
-        ){
+        ) {
 
-            messages[data.id].text =
-            data.text;
+            messages[data.id].text = data.text;
 
-            io.emit(
-                "edit message",
-                data
-            );
+            io.emit("edit message", data);
         }
-    }
-);
 
-socket.on("typing", (username) => {
+    });
 
-    socket.broadcast.emit(
-        "typing",
-        username
-    );
-});
+    socket.on("typing", (username) => {
+
+        socket.broadcast.emit("typing", username);
+
+    });
+
     socket.on("disconnect", () => {
+
         users--;
+
         io.emit("users count", users);
 
         if (socket.username) {
 
-    onlineUsers =
-    onlineUsers.filter(
-        user => user !== socket.username
-    );
+            onlineUsers = onlineUsers.filter(
+                user => user !== socket.username
+            );
 
-    io.emit(
-        "online users",
-        onlineUsers
-    );
+            io.emit("online users", onlineUsers);
 
-    io.emit(
-        "system",
-        socket.username + " left the chat"
-    );
-}
+            io.emit(
+                "system",
+                socket.username + " left the chat"
+            );
+        }
+
     });
+
 });
 
 const PORT = process.env.PORT || 3000;
