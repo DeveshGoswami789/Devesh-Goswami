e173441f26aa0db54cf7c0fd052d580aa14d83d4
DeveshGoswami789 <deveshgiri.goswami123@gmail.com>
2026-08-20 22:05:37 +0530

    Fix Render server binding

--- server.js
+++ server.js
@@ -325,6 +325,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running safely on port ${PORT}`);
 });
